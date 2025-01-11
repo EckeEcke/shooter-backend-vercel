1a2b46a28fd6190aad13debcdf5b7f584f00537b
Christian Eckardt <eckardt.christian@web.de>
2025-01-11 23:34:12 +0100

    remove cors middleware

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require("mongodb")
-const cors = require('cors')
 require('dotenv').config()
 
 let database
@@ -17,50 +16,33 @@ const connectToDatabase = async () => {
   }
 }
 
-const corsMiddleware = cors({ origin: '*' })
-
 const getHighscores = async (req, res) => {
-  corsMiddleware(req, res, async () => {
-    if (req.method === 'GET') {
-      try {
-        await connectToDatabase()
-        const highscores = await database.collection('highscores')
-          .find()
-          .sort({ Score: -1 })
-          .toArray()
-        res.status(200).json(highscores)
-      } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch highscores' })
-      }
-    } else {
-      res.status(405).json({ error: 'Method not allowed' })
-    }
-  })
-}
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
-const postHighscore = async (req, res) => {
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-    res.setHeader('Access-Control-Allow-Origin', '*')
     res.status(204).end()
     return
   }
 
-  corsMiddleware(req, res, async () => {
-    if (req.method === 'POST') {
-      try {
-        await connectToDatabase()
-        const newHighscore = req.body
-        await database.collection('highscores').insertOne(newHighscore)
-        res.status(201).json({ message: 'Highscore added successfully' })
-      } catch (error) {
-        res.status(500).json({ error: 'Failed to add highscore' })
-      }
-    } else {
-      res.status(405).json({ error: 'Method not allowed' })
+  if (req.method === 'GET') {
+    try {
+      await connectToDatabase()
+      const highscores = await database.collection('highscores')
+        .find()
+        .sort({ Score: -1 })
+        .toArray()
+      res.status(200).json(highscores)
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch highscores' })
     }
-  })
+  } else {
+    res.status(405).json({ error: 'Method not allowed' })
+  }
 }
 
-module.exports = { getHighscores, postHighscore }
+const postHighscore = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader
